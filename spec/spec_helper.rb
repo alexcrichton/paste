a9e827431b94712eeeb38ea3b0d1a921f004633f
Alex Crichton <alex@alexcrichton.com>
2010-08-03 17:52:00 -0700

    Rename environment variable correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'bundler'
 
-ENV['BUNDLER_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
 Bundler.setup :default, :test
 
 require 'fileutils'
