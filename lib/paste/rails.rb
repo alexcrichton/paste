8b926a83cf96936e5dccf5c0c994740d5859f796
Alex Crichton <alex@alexcrichton.com>
2010-10-25 22:58:13 -0400

    Using attr_accessor_with_default caused the block to always be called when Paste::Rails.glue was called causing the parsing to always happen for every request on every paste.
    This is obviously very undesirable so fix that by just assigning it through a mattr_accessor

--- lib/paste/rails.rb
+++ lib/paste/rails.rb
@@ -1,15 +1,11 @@
-require 'active_support/core_ext/module/attr_accessor_with_default'
-
 module Paste
   module Rails
     autoload :Helper,  'paste/rails/helper'
     autoload :Railtie, 'paste/rails/railtie'
     autoload :Updater, 'paste/rails/updater'
 
-    class << self
-      attr_accessor_with_default :glue do
-        Paste::Glue.new
-      end
-    end
+    mattr_accessor :glue
+
+    self.glue = Paste::Glue.new
   end
 end
